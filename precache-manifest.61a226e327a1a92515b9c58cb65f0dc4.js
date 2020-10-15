563985256f22b11116f06fda44a801e452e865aa
polhek <ziga.groselj@gmail.com>
2020-10-15 18:37:01 +0200

    Updates

---
+++ precache-manifest.61a226e327a1a92515b9c58cb65f0dc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde7d8b1c5c8d8cf3c520c694dec07b6",
+    "url": "/CV-Generator/index.html"
+  },
+  {
+    "revision": "777ba47f588c99af3868",
+    "url": "/CV-Generator/static/css/2.63fc35d0.chunk.css"
+  },
+  {
+    "revision": "d46f7cd925869effa169",
+    "url": "/CV-Generator/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "777ba47f588c99af3868",
+    "url": "/CV-Generator/static/js/2.d4905024.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CV-Generator/static/js/2.d4905024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46f7cd925869effa169",
+    "url": "/CV-Generator/static/js/main.a1edf4c2.chunk.js"
+  },
+  {
+    "revision": "b4db930aec83ef412e12",
+    "url": "/CV-Generator/static/js/runtime-main.f25098b9.js"
+  }
+]);
